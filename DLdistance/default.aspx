3d7ef2b608543ba869e8bedd905ba9080462b7e2
Krash66 <tomkarasch@tomtekdev.com>
2017-05-14 00:17:50 -0400

    Correctly working version. Bootstrap added, but implemented

--- DLdistance/default.aspx
+++ DLdistance/default.aspx
@@ -6,18 +6,16 @@
 <head runat="server">
     <title>Damerau Levenstein Distance between two words</title>
 </head>
-<body>
-    <h2>Find the D-L Distance between two words</h2>
-    <div id="formdiv" style="align-content: center">
-        <form id="form1" runat="server" style="position: center">
+<body style="width: 770px; text-align: center; margin-left: 361px; background-color: #66CCFF">
+    <h2 style="text-align: center; color: #0000FF;">Find the D-L Distance between two words</h2>
+        <form id="form1" runat="server" style="border: thick solid #FFFFFF; position: center; height: 159px; margin-top: 29px; background-color: #FFFFFF; clip: rect(10px, auto, auto, auto); color: #0000FF;">
             <label>Word 1: </label>
             <asp:TextBox ID="word1" runat="server"></asp:TextBox><br />
             <label>Word 2: </label>
             <asp:TextBox ID="word2" runat="server"></asp:TextBox><br />
             <asp:Button ID="submitButton" runat="server" Text="Calculate" OnClick="Submit_Form1" /><br />
             <label>Answer : </label>
-            <asp:TextBox ID="Answer" length="300px" runat="server"></asp:TextBox>
+            <asp:TextBox ID="Answer" length="300px" runat="server" Height="25px" Rows="2" Width="669px"></asp:TextBox>
         </form>
-    </div>
 </body>
 </html>
--- DLdistance/default.aspx.cs
+++ DLdistance/default.aspx.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+
 
 namespace DLdistance
 {
@@ -22,9 +18,48 @@ namespace DLdistance
         }
         public static string CalculateDL(string w1, string w2)
         {
+            //    ' ... Here is the algorithm  ... converted this to C# below
+            //Function DL_EditDistance(ByVal CharStr1 As Char(), ByVal CharStr2 As Char()) As Integer
+            //
+            //    'int DamerauLevenshteinDistance(char str1[1..lenStr1], char str2[1..lenStr2])
+            //
+            //    '   d is a table with lenStr1+1 rows and lenStr2+1 columns
+            //    '   declare int d[0..lenStr1, 0..lenStr2]
+            //
+            //    '   /i and j are used to iterate over str1 and str2
+            //    '   declare int i, j, cost
+            //
+            //    '   for i from 0 to lenStr1
+            //    '       d[i, 0] := i
+            //    '   LOOP
+            //
+            //    '   for j from 1 to lenStr2
+            //    '       d[0, j] := j
+            //    '   LOOP
+            //
+            //    '   for i from 1 to lenStr1
+            //    '       for j from 1 to lenStr2
+            //    '           if str1[i] = str2[j] then cost := 0
+            //    '                                else cost := 1
+            //    '           d[i, j] := minimum(
+            //    '                                d[i-1, j  ] + 1,     // deletion
+            //    '                                d[i  , j-1] + 1,     // insertion
+            //    '                                d[i-1, j-1] + cost   // substitution
+            //    '                            )
+            //    '           if(i > 1 and j > 1 and str1[i] = str2[j-1] and str1[i-1] = str2[j]) then
+            //    '               d[i, j] := minimum(
+            //    '                                d[i, j],
+            //    '                                d[i-2, j-2] + cost   // transposition
+            //    '                             )
+            //    '   This is where we LOOP
+            //
+            //    '   return d[lenStr1, lenStr2]
+            //
+            //    End Function
+            //
             try
             {
-                // The result
+                // The result asa string to be returned to the form
                 var Distance = "";
                 // Matrix for calculations
                 int[,] Dmatrix = new int[w1.Length + 1, w2.Length + 1];
@@ -38,12 +73,12 @@ namespace DLdistance
                 W2CharArr = w2.ToCharArray();
 
                 // initialize matrix rows
-                for (i = 0; i < w1.Length; i++)
+                for (i = 0; i <= w1.Length; i++)
                 {
                     Dmatrix[i, 0] = i;
                 };
                 // initialize matrix Columns
-                for (j = 0; j < w2.Length; j++)
+                for (j = 1; j <= w2.Length; j++)
                 {
                     Dmatrix[0, j] = j;
                 };
@@ -54,7 +89,7 @@ namespace DLdistance
                     for (j = 1; j <= w2.Length; j++)
                     {
                         // compare characters and determine initial cost
-                        if (W1CharArr[i - 1] == W2CharArr[j - 1])
+                        if (W1CharArr[i-1] == W2CharArr[j-1])
                         {
                             cost = 0;
                         }
@@ -63,14 +98,14 @@ namespace DLdistance
                             cost = 1;
                         };
                         // set initial minimum by comparing deletion and insertion
-                        min1 = System.Math.Min(Dmatrix[i - 1, j], Dmatrix[i, j - 1]);
+                        min1 = System.Math.Min(Dmatrix[i - 1, j] + 1, Dmatrix[i, j - 1] + 1);
                         // Set Dmatrix value by comparing result above with substitution
                         Dmatrix[i, j] = System.Math.Min(min1, Dmatrix[i - 1, j - 1] + cost);
                         // Now compare and try transposition
                         // This is the Damerau addition to the Levenshtein Distance
-                        if (i > 1 && j > 1)
+                        if ((i > 1) & (j > 1))
                         {
-                            if (W1CharArr[i - 1] == W2CharArr[j - 2] && W1CharArr[i - 2] == W2CharArr[j - 1])
+                            if ((W1CharArr[i - 1] == W2CharArr[j - 2]) & (W1CharArr[i - 2] == W2CharArr[j - 1]))
                             {
                                 Dmatrix[i, j] = System.Math.Min(Dmatrix[i, j], Dmatrix[i - 2, j - 2] + cost);
                             };
@@ -78,7 +113,7 @@ namespace DLdistance
                     };
                 };
                 // The Damerau-Levenshtein Distance is now the last entry in the Matrix
-                Distance = "The D-L Distance between " + w1 + "and" + w2 + " is: " + Dmatrix[w1.Length, w2.Length].ToString();
+                Distance = "The D-L Distance between " + w1 + " and " + w2 + " is: " + Dmatrix[w1.Length, w2.Length].ToString();
                 return Distance;
             }
             catch (System.Exception)
